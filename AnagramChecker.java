9a24575c973ad7f78fc6a1f40a6f79b373284cb0
Max Barker <maxbarker26@gmail.com>
2025-09-13 17:52:43 -0800

    Add method stubs.

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -1,5 +1,23 @@
 package assign04;
 
+import java.util.Comparator;
+
 public class AnagramChecker {
 
+	public static String sort(String string) {
+		return null;
+	}
+	
+	public static <T> void insertionSort(T[] arr, Comparator<? super T> cmp) {
+		
+	}
+	
+	public static String[] getLargestAnagramGroup(String[] stringArray) {
+		return null;
+	}
+	
+	public static String[] getLargestAnagramGroup(String filename) {
+		return null;
+	}
 }
+
